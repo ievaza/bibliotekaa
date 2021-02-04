72c8ead7548a564740ee610a1411e7e31865d2f8
ievaza <ievaza@users.noreply.github.com>
2021-02-04 20:41:16 +0200

    style bootstrap

--- assets/app.js
+++ assets/app.js
@@ -1,15 +1,8 @@
-
-import './styles/app.scss';
-
-import './bootstrap';
-
-// start the Stimulus application
-// import './bootstrap';
-
 import './styles/app.scss';
 import 'bootstrap';
+
 const $ = require('jquery');
 const summernote = require('summernote');
 
 $('#summernote').summernote();
-console.log('summernote');
+console.log('wow');
